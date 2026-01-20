57dff7334f4166db07a03150ba2009554051b337
Zhenissuly Aldiyar <119502710+NeoN1X337@users.noreply.github.com>
2026-01-20 17:38:59 +0500

    Refactor Firebase config to use only environment variables
    Removed fallback values for Firebase configuration.

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyDHuH4crihJVWZAIp-jknNNzVF34tGhvck",
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "tengeflow.firebaseapp.com",
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "tengeflow",
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "tengeflow.firebasestorage.app",
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "474142394112",
-    appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:474142394112:web:aafae4e548e5261edb55e5"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
